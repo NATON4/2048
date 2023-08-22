92c9d588bd67f057914fa6ce8a4f9b0379e8c45c
Anton Hrygorenko <a.o.hryhorenko.fitis21@chdtu.edu.ua>
2023-08-22 15:59:07 +0300

    Added main functions

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const container = document.querySelector(".container");
 const coverScreen = document.querySelector(".cover-screen");
 const result = document.getElementById("result");
 const overText = document.getElementById("cover-screen__over-text");
+const rows = 4;
+const columns = 4;
 
 let matrix;
 let score;
-let rows = 4;
-let columns = 4;
 
 function createGrid() {
 
@@ -21,6 +21,7 @@ function createGrid() {
         }
     }
 }
+
 function adjacentCheck(matrix) {
     const rows = matrix.length;
     const columns = matrix[0].length;
@@ -28,7 +29,6 @@ function adjacentCheck(matrix) {
     for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
         for (let columnIndex = 0; columnIndex < columns - 1; columnIndex++) {
             if (matrix[rowIndex][columnIndex] === matrix[rowIndex][columnIndex + 1]) {
-
                 return true;
             }
         }
@@ -37,7 +37,6 @@ function adjacentCheck(matrix) {
     for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
         for (let rowIndex = 0; rowIndex < rows - 1; rowIndex++) {
             if (matrix[rowIndex][columnIndex] === matrix[rowIndex + 1][columnIndex]) {
-                
                 return true;
             }
         }
@@ -45,7 +44,8 @@ function adjacentCheck(matrix) {
 
     return false;
 }
-function possibleMovesCheck() {
+
+function isPossibleMovesCheck() {
     for (let rowIndex in matrix) {
         if (adjacentCheck(matrix[rowIndex])) {
             return true;
@@ -54,10 +54,11 @@ function possibleMovesCheck() {
 
     return false;
 }
+
 function randomPosition(arr) {
-    
     return Math.floor(Math.random() * arr.length);
 }
+
 function hasEmptyBox(){
 
     for (let rowIndex in matrix) {
@@ -67,10 +68,12 @@ function hasEmptyBox(){
             }
         }
     }
+
     return false;
 }
+
 function gameOverCheck() {
-    if (!possibleMovesCheck()) {
+    if (!isPossibleMovesCheck()) {
         coverScreen.classList.remove("hide");
         container.classList.add("hide");
         overText.classList.remove("hide");
@@ -78,10 +81,12 @@ function gameOverCheck() {
         startButton.innerText = "Restart Game";
     }
 }
-const generateRandomCell = () => {
-    const generateRandomNumber = () => {
-        return Math.random() < 0.5 ? 2 : 4;
-    };
+
+function generateRandomNumber() {
+    return Math.random() < 0.5 ? 2 : 4;
+}
+
+function generateRandomCell() {
 
     if (hasEmptyBox()) {
         let randomRow = randomPosition(matrix);
@@ -89,19 +94,25 @@ const generateRandomCell = () => {
 
         if (matrix[randomRow][randomCol] === 0) {
             const randomNumber = generateRandomNumber();
+            const currentCell = document.querySelector(`[data-position = '${randomRow}_${randomCol}']`);
             matrix[randomRow][randomCol] = randomNumber;
-            let element = document.querySelector(`[data-position = '${randomRow}_${randomCol}']`);
-            element.innerHTML = randomNumber;
-            element.classList.add(`box_value-${randomNumber}`);
+            currentCell.innerHTML = randomNumber;
+            currentCell.classList.add(`box_value-${randomNumber}`);
         } else {
             generateRandomCell();
         }
     } else {
         gameOverCheck();
     }
-};
-const removeZero = (arr) => arr.filter((num) => num);
-const checker = (arr, reverseArr = false) => {
+}
+
+function removeZero(arr) {
+    return arr.filter(function(num) {
+        return num;
+    });
+}
+
+function sumAndFillCells(arr, reverseArr = false) {
     arr = reverseArr ? removeZero(arr).reverse() : removeZero(arr);
 
     for (let i = 0; i < arr.length - 1; i++) {
@@ -125,8 +136,19 @@ const checker = (arr, reverseArr = false) => {
     }
     
     return arr;
-};
-const startGame = () => {
+}
+
+/*function fillMatrix(matrix, value, rows, columns) {
+    for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
+        for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
+            matrix[rowIndex][columnIndex] = value;
+        }
+    }
+    
+    return matrix;
+}*/
+
+function startGame() {
     score = 0;
     document.getElementById("score").innerText = score;
     board.innerHTML = "";
@@ -141,34 +163,45 @@ const startGame = () => {
     createGrid();
     generateRandomCell();
     generateRandomCell();
-};
+}
+
 startButton.addEventListener("click", () => {
     startGame();
 });
-const slideDown = () => {
+
+function slideDown() {
     for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
         let num = [];
+
         for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
-            num.push(matrix[rowIndex][columnIndex]);
+            let indices = matrix[rowIndex][columnIndex];
+            num.push(indices);
         }
-        num = checker(num, true);
+        
+        num = sumAndFillCells(num, true);
+        
         for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
             matrix[rowIndex][columnIndex] = num[rowIndex];
-            let element = document.querySelector(`[data-position='${rowIndex}_${columnIndex}']`);
+            const element = document.querySelector(`[data-position='${rowIndex}_${columnIndex}']`);
             element.innerHTML = matrix[rowIndex][columnIndex] ? matrix[rowIndex][columnIndex] : "";
             element.classList.value = "";
             element.classList.add("box", `box_value-${matrix[rowIndex][columnIndex]}`);
         }
     }
     generateRandomCell();
-};
-const slideUp = () => {
+}
+
+function slideUp() {
     for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
         let num = [];
+        
         for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
-            num.push(matrix[rowIndex][columnIndex]);
+            let indices = matrix[rowIndex][columnIndex];
+            num.push(indices);
         }
-        num = checker(num);
+        
+        num = sumAndFillCells(num);
+        
         for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
             matrix[rowIndex][columnIndex] = num[rowIndex];
             const element = document.querySelector(`[data-position = '${rowIndex}_${columnIndex}']`);
@@ -178,14 +211,19 @@ const slideUp = () => {
         }
     }
     generateRandomCell();
-};
-const slideRight = () => {
+}
+
+function slideRight() {
     for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
         let num = [];
+        
         for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
-            num.push(matrix[rowIndex][columnIndex]);
+            let indices = matrix[rowIndex][columnIndex];
+            num.push(indices);
         }
-        num = checker(num, true);
+        
+        num = sumAndFillCells(num, true);
+        
         for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
             matrix[rowIndex][columnIndex] = num[columnIndex];
             let element = document.querySelector(`[data-position = '${rowIndex}_${columnIndex}']`);
@@ -195,14 +233,19 @@ const slideRight = () => {
         }
     }
     generateRandomCell();
-};
-const slideLeft = () => {
+}
+
+function slideLeft() {
     for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
         let num = [];
+        
         for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
-            num.push(matrix[rowIndex][columnIndex]);
+            let indices = matrix[rowIndex][columnIndex];
+            num.push(indices);
         }
-        num = checker(num);
+        
+        num = sumAndFillCells(num);
+        
         for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
             matrix[rowIndex][columnIndex] = num[columnIndex];
             let element = document.querySelector(`[data-position = '${rowIndex}_${columnIndex}']`);
@@ -212,7 +255,8 @@ const slideLeft = () => {
         }
     }
     generateRandomCell();
-};
+}
+
 document.addEventListener("keydown", (e) => {
     if (e.code === "ArrowLeft") {
         slideLeft();
@@ -225,3 +269,32 @@ document.addEventListener("keydown", (e) => {
     }
     document.getElementById("score").innerText = score;
 });
+
+function continuouslyChangeBackgroundColors() {
+    const colorMap = {
+        'box_value-2': '#eee4da',
+        'box_value-4': '#eee1c9',
+        'box_value-8': '#f3b27a',
+        'box_value-16': '#f69664',
+        'box_value-32': '#f77c5f',
+        'box_value-64': '#f75f3b',
+        'box_value-128': '#edd073',
+        'box_value-256': '#edcc63',
+        'box_value-512': '#edc651',
+        'box_value-1024': '#eec744',
+        'box_value-2048': '#ecc230'
+    };
+
+    setInterval(() => {
+        for (const className in colorMap) {
+            const elements = document.getElementsByClassName(className);
+            const color = colorMap[className];
+
+            for (const element of elements) {
+                element.style.backgroundColor = color;
+            }
+        }
+    }, 5);
+}
+
+continuouslyChangeBackgroundColors();
